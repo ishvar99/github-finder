2aa147fbd8fac170f36f98411ffd0c30466557ee
ishvar99 <ishanvarshney99@gmail.com>
2020-06-01 20:41:00 +0530

    moved state logic to app.js

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,38 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        login: 'mojombo',
-        id: 1,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'defunkt',
-        id: 2,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        login: 'pjhyett',
-        id: 3,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
-  render() {
-    const { users } = this.state;
-    return (
-      <div className='flex flex-wrap w-9/12 mx-auto my-24'>
-        {users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
-}
+
+const Users = ({ users }) => {
+  return (
+    <div className='flex flex-wrap w-9/12 mx-auto my-24'>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
 
 export default Users;
